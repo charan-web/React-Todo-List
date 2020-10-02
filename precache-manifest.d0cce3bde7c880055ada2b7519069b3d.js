4b1f622e23788e55c4876fb1a0aa87149e82034e
charan-web <cc710788@gmail.com>
2020-10-02 19:39:30 +0530

    Updates

---
+++ precache-manifest.d0cce3bde7c880055ada2b7519069b3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae13a40c9458464718a9c937763c73d5",
+    "url": "/ReactTodoList/index.html"
+  },
+  {
+    "revision": "5c9551fcc4bd6bad6cbd",
+    "url": "/ReactTodoList/static/css/main.1328adc2.chunk.css"
+  },
+  {
+    "revision": "f95b4cb36f03f2dc0511",
+    "url": "/ReactTodoList/static/js/2.3332b24c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTodoList/static/js/2.3332b24c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c9551fcc4bd6bad6cbd",
+    "url": "/ReactTodoList/static/js/main.e68ca704.chunk.js"
+  },
+  {
+    "revision": "9074831e2b5fa393c544",
+    "url": "/ReactTodoList/static/js/runtime-main.807f7d2c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactTodoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
